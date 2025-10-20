2521071c7c5c0af1cf52b16db2fc2cde6c6e6738
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-10-20 11:26:14 -0600

    feat(main): organice los routes de la api y los middlewares en diferentes archivos -ahora es mas escalable

--- main.py
+++ main.py
@@ -1,50 +1,18 @@
-from fastapi import FastAPI, WebSocket, WebSocketDisconnect, APIRouter
-from fastapi.middleware.cors import CORSMiddleware
-from src.api.presentation.websocket.connection_manager import ConnectionManager
-from src.api.presentation.websocket.chat_handler import handle_request, validate_user_input, handle_rag_response
 import warnings
+from src.api.routes import websocket_routes
+from src.middlewares.cors import add_cors_middleware
+from fastapi import FastAPI
 
 warnings.filterwarnings("ignore", category=UserWarning)
 
 app = FastAPI(
     title="AI Chat API",
     docs_url="/docs",
-    description="This API allows you to chat with an AI model using WebSocket connections.",
+    description="ChatBot de servicio al cliente con base de conocimiento",
     version="1.0.0",
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+add_cors_middleware(app)
 
-router = APIRouter(prefix="/chat", tags=["Chat"])
-manager = ConnectionManager()
-
-
-@router.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    try:
-        await manager.connect(websocket)
-
-        while True:
-            user_input = await handle_request(websocket)
-            print("User: ", user_input)
-
-            puede_procesar = await validate_user_input(user_input, manager, websocket)
-
-            if not puede_procesar:
-                continue
-
-            await handle_rag_response(user_input, manager, websocket)
-
-    except WebSocketDisconnect as e:
-        print("Cliente desconectado")
-        manager.disconnect(websocket)
-    finally:
-        await websocket.close()
-
-app.include_router(router)
+#webSocket
+app.include_router(websocket_routes.router)
---
+++ src/api/routes/websocket_routes.py
@@ -0,0 +1,28 @@
+from fastapi import WebSocket, WebSocketDisconnect, APIRouter
+from src.api.presentation.websocket.connection_manager import ConnectionManager
+from src.api.presentation.websocket.chat_handler import handle_request, validate_user_input, handle_rag_response
+
+router = APIRouter(prefix="/chat", tags=["Chat"])
+manager = ConnectionManager()
+
+@router.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    try:
+        await manager.connect(websocket)
+
+        while True:
+            user_input = await handle_request(websocket)
+            print("User: ", user_input)
+
+            puede_procesar = await validate_user_input(user_input, manager, websocket)
+
+            if not puede_procesar:
+                continue
+
+            await handle_rag_response(user_input, manager, websocket)
+
+    except WebSocketDisconnect as e:
+        print("Cliente desconectado")
+        manager.disconnect(websocket)
+    finally:
+        await websocket.close()
