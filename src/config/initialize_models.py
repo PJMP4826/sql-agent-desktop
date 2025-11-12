6a3aab6f82dc760a196aeb834512a5af7f771c12
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-11-12 12:39:13 -0600

    fix(config): add typing correctos a las funciones de initialize_models.py

--- src/config/initialize_models.py
+++ src/config/initialize_models.py
@@ -6,22 +6,22 @@ from llama_index.embeddings.google_genai import GoogleGenAIEmbedding
 
 load_dotenv()
 
-API_KEY = os.getenv("GOOGLE_API_KEY")
-EMBED_MODEL_NAME: str = os.getenv("EMBED_MODEL_NAME")
-LLM_MODEL: str = os.getenv("LLM_MODEL")
+API_KEY: str = os.getenv("GOOGLE_API_KEY") or ""
+EMBED_MODEL_NAME: str = os.getenv("EMBED_MODEL_NAME") or ""
+LLM_MODEL: str = os.getenv("LLM_MODEL") or ""
 
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT")
-DB_HOST = os.getenv("DB_HOST")
+DB_USER: str = os.getenv("DB_USER") or ""
+DB_PASSWORD: str = os.getenv("DB_PASSWORD") or ""
+DB_NAME: str = os.getenv("DB_NAME") or ""
+DB_PORT: str = os.getenv("DB_PORT") or ""
+DB_HOST: str = os.getenv("DB_HOST") or ""
 
 
-def validate_env(env_requeridas: list[str]):
-    faltantes = []
+def validate_env(env_requeridas: list[str]) -> None:
+    faltantes: list[str] = []
 
     for var in env_requeridas:
-        value = os.getenv(var)
+        value = os.getenv(var) or None
         if value is None or value.strip() == "":
             faltantes.append(var)
 
@@ -52,4 +52,3 @@ def initialize_db_credentials() -> dict:
         "db_port": DB_PORT,
         "db_host": DB_HOST,
     }
-
