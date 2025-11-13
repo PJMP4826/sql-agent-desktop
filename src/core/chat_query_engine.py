69ee39a3d9579bbbaae194e5cd7c967437c8d067
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-11-12 19:56:56 -0600

    feat(query_engine): add method para crear un Storage Context en disco para evitar recalcular embeddings -tambien se agregan try-except para debbuging

--- src/core/chat_query_engine.py
+++ src/core/chat_query_engine.py
@@ -8,9 +8,10 @@ from src.config.initialize_models import initialize_models, initialize_db_creden
 from llama_index.core.memory import ChatSummaryMemoryBuffer
 from llama_index.core.base.llms.types import ChatMessage, MessageRole
 from sqlalchemy import create_engine
-from llama_index.core import SQLDatabase
+from llama_index.core import SQLDatabase, StorageContext, PromptTemplate
 from llama_index.core.query_engine import NLSQLTableQueryEngine
 from llama_index.core.chat_engine import CondenseQuestionChatEngine
+from pathlib import Path
 
 load_dotenv()
 
@@ -33,68 +34,105 @@ class ChatQueryEngine:
 
         self.system_prompt_rol = rol_bot
 
+    def _create_storage_context(self):
+        self.persist_dir = Path("./storage/sql_index")
+        self.persist_dir.mkdir(parents=True, exist_ok=True)
+
+        docstore_path = self.persist_dir / "docstore.json"
+
+        if not docstore_path.exists():
+            print("Creando nuevo StorageContext (no se encontro uno previo)")
+            self.storage_context = StorageContext.from_defaults()
+        else:
+            print(f"Cargando StorageContext existente desde {self.persist_dir}")
+            self.storage_context = StorageContext.from_defaults(
+                persist_dir=str(self.persist_dir)
+            )
+
     def _initialize_components(self):
-        models = initialize_models()
+        try:
+            models = initialize_models()
 
-        self.llm = models["llm"]
+            self.llm = models["llm"]
 
-        self.embed_model = models["embed_model"]
+            self.embed_model = models["embed_model"]
 
-        self.memory = ChatSummaryMemoryBuffer.from_defaults(
-            llm=self.llm, token_limit=1500
-        )
+            self.memory = ChatSummaryMemoryBuffer.from_defaults(
+                llm=self.llm, token_limit=2000
+            )
+        except Exception as e:
+            print("Error: ", str(e))
 
     def _initalize_database(self):
-        credentials = initialize_db_credentials()
-        db_user: str = credentials["db_user"]
-        db_password: str = credentials["db_password"]
-        db_name: str = credentials["db_name"]
-        db_port: str = credentials["db_port"]
-        db_host: str = credentials["db_host"]
+        try:
+            credentials = initialize_db_credentials()
+            db_user: str = credentials["db_user"]
+            db_password: str = credentials["db_password"]
+            db_name: str = credentials["db_name"]
+            db_port: str = credentials["db_port"]
+            db_host: str = credentials["db_host"]
 
-        self.db_engine = create_engine(
-            f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?client_encoding=UTF8",
-            echo=False,
-            pool_pre_ping=True,
-            pool_recycle=3600,
-        )
+            self.db_engine = create_engine(
+                f"postgresql+psycopg2://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?client_encoding=UTF8",
+                echo=False,
+                pool_pre_ping=True,
+                pool_recycle=3600,
+            )
 
-        self.include_tables = [
-            "cancelaciones",
-            "cfdisrelacionados",
-            "conceptos",
-            "configuracion",
-            "documentos",
-            "documentosrelpago",
-            "emisor",
-            "impuestos",
-            "pagos",
-            "receptor",
-        ]
+            self.include_tables = [
+                "cancelaciones",
+                "cfdisrelacionados",
+                "conceptos",
+                "configuracion",
+                "documentos",
+                "documentosrelpago",
+                "emisor",
+                "impuestos",
+                "pagos",
+                "receptor",
+            ]
 
-        self.sql_database = SQLDatabase(
-            self.db_engine, include_tables=self.include_tables
-        )
+            self._create_storage_context()
 
-        # natural language a SQL
-        self.sql_query_engine = NLSQLTableQueryEngine(
-            sql_database=self.sql_database,
-            tables=self.include_tables,
-            llm=self.llm,
-            embed_model=self.embed_model,
-            synthesize_response=False,
-            verbose=True,
-        )
+            self.sql_database = SQLDatabase(
+                self.db_engine,
+                include_tables=self.include_tables,
+            )
 
-        # para responder usando el contexto de la conversacion
-        self.chat_engine = CondenseQuestionChatEngine.from_defaults(
-            query_engine=self.sql_query_engine,
-            memory=self.memory,
-            verbose=False,
-            llm=self.llm,
-        )
+            # natural language a SQL
+            self.sql_query_engine = NLSQLTableQueryEngine(
+                sql_database=self.sql_database,
+                tables=self.include_tables,
+                llm=self.llm,
+                embed_model=self.embed_model,
+                synthesize_response=False,
+                verbose=False,
+            )
 
-    def procesar_query(self, query: str) -> Optional[Dict]:
+            self.storage_context.persist(persist_dir=str(self.persist_dir))
+
+            # para responder usando el contexto de la conversacion
+            self.chat_engine = CondenseQuestionChatEngine.from_defaults(
+                query_engine=self.sql_query_engine,
+                memory=self.memory,
+                verbose=False,
+                llm=self.llm,
+            )
+            
+        except Exception as e:
+            print("Error al inicializar components db: ", e)
+
+    def procesar_query(self, query: str) -> Optional[str]:
+        try:
+            response = self.chat_engine.chat(query)
+
+            return str(response)
+        except Exception as e:
+            error_msg = f"Error procesando la consulta: {str(e)}"
+            print(error_msg)
+            return None
+
+    def procesar_query_json(self, query: str) -> Optional[Dict]:
         try:
             response = self.chat_engine.chat(
                 f"""
