c68c1d603f79d9efd236172fc699b13b48854a4d
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-11-28 13:49:14 -0600

    refactor(domain): change SQLQuery al module value_object

--- app/domain/entities/query.py
+++ app/domain/entities/query.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass, field
 from typing import Optional
-from sql_query import SQLQuery
+from app.domain.value_objects.sql_query import SQLQuery
 from datetime import datetime
 
 #representa la consulta del usuario
