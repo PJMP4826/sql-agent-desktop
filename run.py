93289337d92ba1b51314e2d59d9cb47a96d46db8
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-11-29 11:37:32 -0600

    feat: implement run para cargar el servidor FastAPI con uvicorn y lanzar el navegador automaticamente

---
+++ run.py
@@ -0,0 +1,27 @@
+import os
+import threading
+import webbrowser
+import uvicorn
+from main import app
+from dotenv import load_dotenv
+
+load_dotenv()
+
+HOST: str = str(os.getenv("HOST_SERVER"))
+PORT: int = int(os.getenv("PORT_SERVER"))
+URL: str = f"http://{HOST}:{PORT}"
+
+def open_browser():
+    webbrowser.open(URL)
+
+if __name__ == "__main__":
+    threading.Timer(1.0, open_browser).start()
+    
+    
+    uvicorn.run(
+        "main:app", 
+        host=HOST, 
+        port=PORT,
+        reload=True,
+        workers=1 
+    )
