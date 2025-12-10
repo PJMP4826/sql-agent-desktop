7a6bfaa45bb133d00664997d094ecb9803db8327
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-12-10 12:50:57 -0600

    feat(ports): update VectoreStorePort para usar la entidad Document

--- app/application/ports/vector_store_port.py
+++ app/application/ports/vector_store_port.py
@@ -1,14 +1,24 @@
-from typing import List, Any
+from typing import List
 from abc import ABC, abstractmethod
+from app.domain.entities.document import Document
 
 
 class VectorStorePort(ABC):
     @abstractmethod
-    def store_documents(self, documents: List[Any]) -> None:
+    def add_documents(self, documents: List[Document]) -> List[str]:
+        pass
+
+    # buscar documentos
+    @abstractmethod
+    def query(self, query_text: str, top_k: int = 5) -> List[Document]:
         pass
 
     @abstractmethod
-    def query(self, query_text: str, top_k: int = 5) -> List[Any]:
+    def delete_documents(self, ids: List[str]):
+        pass
+
+    @abstractmethod
+    def delete_collection(self) -> bool:
         pass
 
     @abstractmethod
