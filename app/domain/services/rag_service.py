3795944999b6cee24bcef47321e2fde371d39f49
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-12-12 12:12:49 -0600

    fix(rag_service): corrige validation para crear vector store index -fix nombre de la varible system_prompt -add type de retorno del method _create_storage_context() -add parametros faltantes a VectorStoreIndex.from_documents

--- app/domain/services/rag_service.py
+++ app/domain/services/rag_service.py
@@ -1,10 +1,11 @@
+import logging
 from typing import Optional, Any
 from llama_index.core import VectorStoreIndex
 from llama_index.core import StorageContext
 from app.application.ports.vector_store_port import VectorStorePort
 from app.domain.services.document_manager import DocumentManager
 from app.shared.domain_exceptions import DomainException
-from config.settings import Settings
+from app.config.settings import Settings
 from llama_index.core.indices.base import BaseIndex
 from llama_index.core.base.base_query_engine import BaseQueryEngine
 from pathlib import Path
@@ -12,16 +13,19 @@ from llama_index.core.chat_engine.types import ChatMode, BaseChatEngine
 
 from app.application.ports.llm_port import LLMPort
 
+logger = logging.getLogger(__name__)
+
+
 
 class RagService:
     def __init__(
         self,
         vector_store: VectorStorePort,
         llm_client: LLMPort,
-        system_prompt_path: Optional[str],
+        system_prompt: Optional[str],
         auto_index_on_empty: bool = False,
     ) -> None:
-        self.system_prompt_path: Optional[str] = system_prompt_path
+        self.system_prompt: Optional[str] = system_prompt
         self.vector_store = vector_store
         self.settings = Settings()  # type: ignore
 
@@ -33,24 +37,38 @@ class RagService:
 
         self.llm_client = llm_client
 
+        if auto_index_on_empty and self._is_vectore_store_empty():
+                try:
+                    self.index_directory()
+                except Exception as e:
+                    logger.error(f"Auto-indexing failed: {e}")
+
     def _is_vectore_store_empty(self) -> bool:
         try:
-            return self.vector_store.get_collection_count() == 0
+            count:int = self.vector_store.get_collection_count()
+
+            return count == 0
         except Exception:
             return False
 
-    def _create_storage_context(self) -> None:
-        self.storage_context = StorageContext.from_defaults(
+    def _create_storage_context(self) -> StorageContext:
+        return StorageContext.from_defaults(
             vector_store=self.vector_store.get_vector_store
         )
 
-    def _create_index(self):
+    def _create_index(self) -> VectorStoreIndex:
         try:
-            if not self._is_vectore_store_empty():
-                index: VectorStoreIndex = VectorStoreIndex(
-                    nodes=[], storage_context=self.storage_context
+
+            storage_context = self._create_storage_context()
+
+            if self._is_vectore_store_empty():
+                return VectorStoreIndex(
+                    nodes=[], 
+                    storage_context=storage_context,
+                    embed_model=self.llm_client.get_embed_model()
                 )
 
+            # el vector store no es empty
             index: VectorStoreIndex = VectorStoreIndex.from_vector_store(  # type: ignore
                 vector_store=self.vector_store.get_vector_store,
                 embed_model=self.llm_client.get_embed_model(),
@@ -71,9 +89,13 @@ class RagService:
     @property
     def chat_engine(self) -> BaseChatEngine:
         if self._chat_engine is None:
+
+            system_prompt = self.system_prompt or "Eres un asistente contable"
+
             self._chat_engine = self.index.as_chat_engine(  # type: ignore
+                llm=self.llm_client.get_llm_model(),
                 chat_mode=ChatMode.CONDENSE_PLUS_CONTEXT,
-                system_prompt=self.system_prompt_path,
+                system_prompt=system_prompt,
                 verbose=False,
             )
         return self._chat_engine
@@ -82,7 +104,9 @@ class RagService:
     def query_engine(self) -> BaseQueryEngine:
         """Lazy loading del query engine con cache"""
         if self._query_engine is None:
-            self._query_engine = self.index.as_query_engine()  # type: ignore
+            self._query_engine = self.index.as_query_engine( # type: ignore
+                llm=self.llm_client.get_llm_model()
+            )  # type: ignore
         return self._query_engine
 
     def refresh_engines(self):
@@ -120,7 +144,10 @@ class RagService:
             storage_context = self._create_storage_context()
 
             new_index = VectorStoreIndex.from_documents(
-                documents, storage_context=storage_context, show_progress=True
+                documents,
+                storage_context=storage_context,
+                embed_model=self.llm_client.get_embed_model(),
+                show_progress=True,
             )
 
             self._index = new_index
@@ -135,6 +162,7 @@ class RagService:
         except DomainException:
             raise
         except Exception as e:
+            print(str(e))
             raise DomainException(
                 "Error indexing directory",
                 details={"directory": directory, "error": str(e)},
@@ -172,7 +200,7 @@ class RagService:
         """
         try:
 
-            self.vector_store.get_collection_count()
+            self.vector_store.delete_collection()
 
             self.refresh_all()
 
@@ -184,7 +212,7 @@ class RagService:
             raise DomainException(
                 "Error re-indexing documentos", details={"error": str(e)}
             )
-        
+
     def query(self, user_input: str) -> str:
         try:
             response = self.query_engine.query(user_input)
@@ -207,4 +235,3 @@ class RagService:
                 "Error en chat, usando chat_engine",
                 details={"question": user_input, "error": str(e)},
             )
-
