551dc87c07ab46a452a2d2b83a406612c6fdb524
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-12-18 16:13:17 -0600

    refactor(main): change rutas de importacion de los modules de routes de API y CORS

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import warnings
-from src.api.routes import websocket_routes
-from src.api.routes import api_routes
-from src.api.routes import web_routes
-from src.middlewares.cors import add_cors_middleware
+from app.api.routes import websocket_routes
+from app.api.routes import api_routes
+from app.api.routes import web_routes
+from app.api.http.controllers.middlewares.cors import add_cors_middleware
 from fastapi import FastAPI
-from src.core.chat_query_engine import ChatQueryEngine
-from src.api.presentation.websocket.agent_handler import AgentHandler
-from src.cli.main import CLI
 
 
 warnings.filterwarnings("ignore", category=UserWarning)
@@ -18,32 +15,9 @@ app = FastAPI(
     version="1.0.0",
 )
 
-
 add_cors_middleware(app)
 
 # webSocket
 app.include_router(websocket_routes.router)
 app.include_router(api_routes.router)
 app.include_router(web_routes.router, prefix="")
-
-# chat_engine = ChatQueryEngine()
-# chat_engine.run_chat()
-
-# async def main():
-#     try:
-#         agent_handler = AgentHandler()
-#         await agent_handler.run_chat()
-#     except Exception as e:
-#         print(f"\n Error fatal al inicializar: {str(e)}")
-
-
-# if __name__ == "__main__":
-#     asyncio.run(main())
-
-# if __name__ == "__main__":
-
-#     try:
-#         bot = CLI()
-#         bot.main()
-#     except Exception as e:
-#         print(f"Error faltal {e}")
