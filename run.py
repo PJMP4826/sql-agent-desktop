868b8659e646ce0a33eb0cbd4d150816c2d9e392
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2025-12-21 22:21:29 -0600

    refactor(run): organiza el arranque la app en funciones

--- run.py
+++ run.py
@@ -2,29 +2,32 @@ import os
 import threading
 import webbrowser
 import uvicorn
-from main import app # type: ignore
+from main import app  # type: ignore
 from dotenv import load_dotenv
 
 load_dotenv()
 
 HOST: str = str(os.getenv("HOST_SERVER"))
-PORT: int = int(os.getenv("PORT_SERVER"))
+PORT: int = int(os.getenv("PORT_SERVER"))  # type: ignore
 URL: str = f"http://{HOST}:{PORT}"
+INTERVAL = 6.0
+
 
 def open_browser():
     webbrowser.open(URL)
 
+
+def open_browser_interval():
+    threading.Timer(INTERVAL, open_browser).start()
+
+
+def run_server():
+    uvicorn.run("main:app", host=HOST, port=PORT, reload=True, workers=1)
+
+
 if __name__ == "__main__":
     try:
-        threading.Timer(7.0, open_browser).start()
-    
-    
-        uvicorn.run(
-            "main:app", 
-            host=HOST, 
-            port=PORT,
-            reload=True,
-            workers=1 
-        )
+        open_browser_interval()
+        run_server()
     except Exception as e:
         print("Error: ", e)
