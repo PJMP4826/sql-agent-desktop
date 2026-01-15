37f96eaac5d32384eda527ab7c2f60af20bfad57
PJMP4826 <faustojaviermendozaperez.tareas@gmail.com>
2026-01-14 19:50:19 -0600

    refactor(sql-agent): ahora RAG service y limpia factory
    - elimina la creación del RAG fuera del SQLAgentFactory (ya no habra un RAG en la PC del usario)
     - ahora el agent consume un RagClientService que habla con un RAG externo via WebSockets.
    - elimina vector stores y context builders internos
    - SQLQueryTool usa el RAG client directamente

--- app/domain/factories/sql_agent_factory.py
+++ app/domain/factories/sql_agent_factory.py
@@ -1,15 +1,15 @@
 import logging
 from app.domain.agents.sql_agent.sql_agent import SQLAgent
 from app.domain.agents.sql_agent.tools.sql_query_tool import SQLQueryTool
-from app.domain.agents.sql_agent.tools.get_context_conversation import GetContextConversation
-from app.domain.services.context_builder import ContextBuilder
 from app.domain.services.table_classifier import TableClassifier
-from app.domain.factories.rag_factory import RagServiceFactory
 from app.application.ports.llm_port import LLMPort
-from app.application.ports.vector_store_port import VectorStorePort
 from app.infrastructure.database.sql.sql_database_adapter import SQLDatabaseAdapter
 from app.infrastructure.database.sql.connection_manager import ConnetionManager
 from app.config.settings import Settings
+from app.infrastructure.websocket.websocket_client import WebSocketClient
+from app.infrastructure.services.rag_client_service import RagClientService
+from app.config.settings import Settings
+from app.config.api_endpoints import ApiEndpoints
 
 logger = logging.getLogger(__name__)
 
@@ -22,12 +22,10 @@ class SQLAgentFactory:
     def __init__(
         self,
         llm_client: LLMPort,
-        vector_store: VectorStorePort
     ):
         self.llm_client = llm_client
-        self.vector_store = vector_store
     
-    def create_sql_agent(self, agent_name: str = "Asistente Contable") -> SQLAgent:
+    def create_sql_agent(self, settings: Settings, agent_name: str = "Asistente Contable") -> SQLAgent:
         """
         Crear SQLAgent con todas sus dependencias.
         
@@ -40,45 +38,38 @@ class SQLAgentFactory:
         try:
             logger.info("Creating SQL Agent with dependencies...")
             
-            # 1. Crear RAG Factory
-            rag_factory = RagServiceFactory(
-                vector_store=self.vector_store,
-                llm_client=self.llm_client
-            )
+            # Crear herramientas
+            table_classifier = TableClassifier()
+            # context_builder = ContextBuilder(rag_service=general_rag)
             
-            # 2. Crear RAG services para clasificación y contexto
-            classifier_rag = rag_factory.create_classifier_rag()
-            general_rag = rag_factory.create_general_rag()
-            
-            # 3. Crear herramientas
-            table_classifier = TableClassifier(rag_service=classifier_rag)
-            context_builder = ContextBuilder(rag_service=general_rag)
-            
-            settings = Settings() # type: ignore
-
             connection_manager = ConnetionManager(settings=settings)
 
             sql_adapter = SQLDatabaseAdapter(connection_manager=connection_manager)
 
-            # 4. Crear SQL Query Tool
+            api = ApiEndpoints(settings)
+            ws = WebSocketClient(api.chat.get_sql_context)
+            print(f"URL: {api.chat.get_sql_context}")
+
+            rag_service = RagClientService(ws_client=ws)
+
+            # Crear SQL Query Tool
             sql_query_tool = SQLQueryTool(
-                context_builder=context_builder,
-                table_classifier=table_classifier,
-                rag_factory=rag_factory,
                 llm_client=self.llm_client,
                 connection_manager=connection_manager,
-                sql_adapter=sql_adapter
+                sql_adapter=sql_adapter,
+                rag_service=rag_service,
+                table_classifier=table_classifier
             )
 
-            get_context_tool = GetContextConversation(
-                rag_service=general_rag
-            )
+            # get_context_tool = GetContextConversation(
+            #     rag_service=rag_service
+            # )
             
-            # 5. Crear SQL Agent
+            # Crear SQL Agent
             sql_agent = SQLAgent(
                 llm_client=self.llm_client,
                 sql_query_tool=sql_query_tool,
-                get_context_tool=get_context_tool,
+                # get_context_tool=get_context_tool,
                 agent_name=agent_name
             )
             
